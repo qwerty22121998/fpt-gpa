803047a7ebcc9d47791c93e7c13b12485f67c405
VuHK <qwerty22121998@gmail.com>
2019-06-04 23:48:02 +0700

    remove locale, optimize code

--- js/gpa.js
+++ js/gpa.js
@@ -1,17 +1,15 @@
 
-const blackList = ["OJS","VOV","GDQP"]
+const blackList = ["OJS", "VOV", "GDQP", "PRO001"]
 
 const record = $(".table.table-hover>tbody:first>tr")
 
-const semesterList = (record) => record.find("td:nth-child(3)").
-    map((i, e) => e.firstChild).
-    map((i, e) => e.textContent).toArray().
+const semesterList = (record) => record.find("td:nth-child(3)").toArray().
+    filter(e => e.firstChild !== null).map(e=>e.firstChild.textContent).
     map(e => {
         let h = e.length / 2;
         return e.slice(0, h) !== e.slice(h) ? e : e.slice(0, h)
     }).
     filter((e, i, a) => a.indexOf(e) === i)
-
 
 const subjList = (record, blackList) => record.toArray().
     map((e) => ({
@@ -21,18 +19,17 @@ const subjList = (record, blackList) => record.toArray().
         "credit": e.childNodes[6].textContent,
         "grade": e.childNodes[7].textContent,
         "status": e.childNodes[8].textContent,
-    })).filter(e => e.grade).filter(e => !blackList.some(prefix => e.subject_code.includes(prefix)))
+    })).filter(e => e.term !== "0" && !blackList.some(prefix => e.subject_code.includes(prefix)) && e.grade && e.status == "Passed")
+
+const semSubList = (sem, sub) => sem.map(sem => ({
+    "semester": sem,
+    "list": sub.filter(sub => sub.semester == sem)
+}))
+// .filter(semsub => semsub.list.length)
 
 
-
-const semSubList = (sem, sub) => {
-    return sem.map(sem => ({
-        "semester": sem,
-        "list": sub.filter(sub => sub.semester == sem)
-    }))
-}
-
-const avg = sem => sem.list.map(sem => sem.grade*sem.credit).reduce((a, b) => a - -b, 0) / sem.list.map(sem => sem.credit).reduce((a, b) => a - -b, 0)
+const avg = sem => sem.list.map(sem => sem.grade * sem.credit).
+    reduce((a, b) => a - -b, 0) / sem.list.map(sem => sem.credit).reduce((a, b) => a - -b, 0)
 
 const semSubAvg = semsub => {
     return semsub.map(semsub => {
@@ -40,3 +37,6 @@ const semSubAvg = semsub => {
         return semsub
     })
 }
+
+
+let data = semSubAvg(semSubList(semesterList(record), subjList(record, blackList)))
