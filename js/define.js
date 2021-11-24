7652f982b94230214883bde43757ab980bd2c15f
huong <dinhlequynhhuong0805@gmail.com>
2021-11-24 22:55:25 +0700

    finish

--- js/define.js
+++ js/define.js
@@ -1,3 +1,11 @@
+const SemIndex = {
+  Spring: 0,
+  Summer: 1,
+  Fall: 2,
+};
+
+const DefaultNonGPA = ["OJS", "VOV", "GDQP", "LAB", "ENT", "SSS", "ĐNH"];
+
 class Subject {
   semester;
   code;
@@ -14,11 +22,12 @@ class Subject {
     this.status = status;
   }
 
-  toString() {
-    return JSON.stringify(this);
-  }
-
-  isGPA() {
+  includeInGPA() {
+    for (let code of nonGPAList) {
+      if (this.code.startsWith(code)) {
+        return false;
+      }
+    }
     return this.credit != 0;
   }
 
@@ -28,13 +37,13 @@ class Subject {
 
   pointLabel() {
     if (this.status == "Not passed") return "label-danger";
-    if (this.isGPA()) {
+    if (this.includeInGPA()) {
       return "label-success";
     }
     return "label-default";
   }
 
-  render() {
+  DOM() {
     const div = createHTML(`<div class="subject-block"/>`);
     div.append(
       createHTML(
@@ -45,7 +54,7 @@ class Subject {
       )
     );
 
-    if (this.isGPA())
+    if (this.includeInGPA())
       div.append(
         createHTML(
           `<span class="label point ${rankLabel(this.grade)}">${this.grade} x ${
@@ -66,32 +75,78 @@ class Semester {
     this.year = name.slice(-4);
   }
 
-  gpa() {
+  get Year() {
+    return this.year;
+  }
+  get Gpa() {
     const avg = this.subjects.reduce(
-      (avg, subj) => ({
-        sum: avg.sum + subj.grade * subj.credit,
-        total: avg.total + subj.credit,
-      }),
+      (avg, subj) => {
+        if (subj.includeInGPA() && subj.status == "Passed")
+          return {
+            sum: avg.sum + subj.grade * subj.credit,
+            total: avg.total + subj.credit,
+          };
+        return avg;
+      },
       {
         sum: 0,
         total: 0,
       }
     );
-    const gpa = avg.sum / avg.total;
+    if (avg.total == 0) {
+      return createHTML(`<span class="label label-default">No Data</span>`);
+    }
+    const gpa = round(avg.sum / avg.total);
     return createHTML(`<span class="label ${rankLabel(gpa)}">${gpa}</span>`);
   }
 
   get Semester() {
-    return createHTML(
-      `<span class="label label-primary">${this.semester}</span>`
-    );
+    switch (this.semester) {
+      case "Summer":
+        return createHTML(`<span class="label label-warning">Summer</span>`);
+      case "Spring":
+        return createHTML(`<span class="label label-success">Spring</span>`);
+      case "Fall":
+        return createHTML(`<span class="label label-info">Fall</span>`);
+    }
   }
 
   get Subjects() {
     const div = document.createElement("div");
     this.subjects.forEach((sub) => {
-      div.append(sub.render());
+      div.append(sub.DOM());
     });
     return div;
+  }
+}
+
+class GPATable {
+  table = null;
+  header = null;
+  body = null;
+  constructor() {
+    this.table = createHTML(
+      `<table id="gpa-table" class="table table-hover"/>`
+    );
+    this.header = this.table.createTHead();
+    this.body = this.table.createTBody();
+  }
+
+  addHeader(...cells) {
+    const row = this.header.insertRow();
+    cells.forEach((e) => {
+      row.insertCell().outerHTML = `<th>${e}</th>`;
+    });
+  }
+
+  addRow(...cells) {
+    const row = this.body.insertRow();
+    cells.forEach((e) => {
+      row.insertCell().append(e);
+    });
+  }
+
+  DOM() {
+    return this.table;
   }
 }
--- js/util.js
+++ js/util.js
@@ -1,9 +1,3 @@
-const SemIndex = {
-  Spring: 0,
-  Summer: 1,
-  Fall: 2,
-};
-
 const extractText = (node) => {
   if (node.childElementCount == 0) {
     return node.innerHTML;
@@ -25,40 +19,11 @@ const createElement = (root, ...elems) => {
   return div;
 };
 
-class TableRender {
-  table = null;
-  header = null;
-  body = null;
-  constructor() {
-    this.table = createHTML(
-      `<table id="gpa-table" class="table table-hover"/>`
-    );
-    this.header = this.table.createTHead();
-    this.body = this.table.createTBody();
-  }
-
-  addHeader(...cells) {
-    const row = this.header.insertRow();
-    cells.forEach((e) => {
-      row.insertCell().outerHTML = `<th>${e}</th>`;
-    });
-  }
-
-  addRow(...cells) {
-    const row = this.body.insertRow();
-    cells.forEach((e) => {
-      row.insertCell().append(e);
-    });
-  }
-
-  render() {
-    return this.table;
-  }
-}
-
 const rankLabel = (grade) => {
   if (grade >= 9) return "label-warning";
   if (grade >= 8) return "label-primary";
   if (grade >= 5) return "label-info";
   return "label-danger";
 };
+
+const round = (num) => Math.round(num * 100) / 100;
